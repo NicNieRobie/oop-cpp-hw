af689c63cd2d5a461dcc6a6e5435f7fb666b536f
Vladislav Pendishchuk <vmpendischuk@edu.hse.ru>
2021-10-23 03:31:21 +0300

    Folder hierarchy changes, explanatory note added

---
+++ program/cmake-build-debug/CMakeFiles/oop_cpp_hw.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Vlad/CLionProjects/oop-cpp-hw/program/container.cpp" "C:/Users/Vlad/CLionProjects/oop-cpp-hw/program/cmake-build-debug/CMakeFiles/oop_cpp_hw.dir/container.cpp.obj"
+  "C:/Users/Vlad/CLionProjects/oop-cpp-hw/program/main.cpp" "C:/Users/Vlad/CLionProjects/oop-cpp-hw/program/cmake-build-debug/CMakeFiles/oop_cpp_hw.dir/main.cpp.obj"
+  "C:/Users/Vlad/CLionProjects/oop-cpp-hw/program/plane.cpp" "C:/Users/Vlad/CLionProjects/oop-cpp-hw/program/cmake-build-debug/CMakeFiles/oop_cpp_hw.dir/plane.cpp.obj"
+  "C:/Users/Vlad/CLionProjects/oop-cpp-hw/program/rnd.cpp" "C:/Users/Vlad/CLionProjects/oop-cpp-hw/program/cmake-build-debug/CMakeFiles/oop_cpp_hw.dir/rnd.cpp.obj"
+  "C:/Users/Vlad/CLionProjects/oop-cpp-hw/program/ship.cpp" "C:/Users/Vlad/CLionProjects/oop-cpp-hw/program/cmake-build-debug/CMakeFiles/oop_cpp_hw.dir/ship.cpp.obj"
+  "C:/Users/Vlad/CLionProjects/oop-cpp-hw/program/test_generation.cpp" "C:/Users/Vlad/CLionProjects/oop-cpp-hw/program/cmake-build-debug/CMakeFiles/oop_cpp_hw.dir/test_generation.cpp.obj"
+  "C:/Users/Vlad/CLionProjects/oop-cpp-hw/program/timer.cpp" "C:/Users/Vlad/CLionProjects/oop-cpp-hw/program/cmake-build-debug/CMakeFiles/oop_cpp_hw.dir/timer.cpp.obj"
+  "C:/Users/Vlad/CLionProjects/oop-cpp-hw/program/train.cpp" "C:/Users/Vlad/CLionProjects/oop-cpp-hw/program/cmake-build-debug/CMakeFiles/oop_cpp_hw.dir/train.cpp.obj"
+  "C:/Users/Vlad/CLionProjects/oop-cpp-hw/program/transport.cpp" "C:/Users/Vlad/CLionProjects/oop-cpp-hw/program/cmake-build-debug/CMakeFiles/oop_cpp_hw.dir/transport.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ program/cmake-build-debug/CMakeFiles/oop_cpp_hw.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "../bin/oop_cpp_hw.exe"
+  "../bin/oop_cpp_hw.exe.manifest"
+  "../bin/oop_cpp_hw.pdb"
+  "CMakeFiles/oop_cpp_hw.dir/container.cpp.obj"
+  "CMakeFiles/oop_cpp_hw.dir/main.cpp.obj"
+  "CMakeFiles/oop_cpp_hw.dir/plane.cpp.obj"
+  "CMakeFiles/oop_cpp_hw.dir/rnd.cpp.obj"
+  "CMakeFiles/oop_cpp_hw.dir/ship.cpp.obj"
+  "CMakeFiles/oop_cpp_hw.dir/test_generation.cpp.obj"
+  "CMakeFiles/oop_cpp_hw.dir/timer.cpp.obj"
+  "CMakeFiles/oop_cpp_hw.dir/train.cpp.obj"
+  "CMakeFiles/oop_cpp_hw.dir/transport.cpp.obj"
+  "liboop_cpp_hw.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oop_cpp_hw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
